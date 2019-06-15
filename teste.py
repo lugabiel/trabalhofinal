c83eb62ef7d80efe809af1ad626274aa9e09496e
Pedro Ian <pedroianmoraesus@gmail.com>
2019-06-15 13:50:02 -0300

    Update teste.py
    Atualizado o fitness para conjunto upas
    Alteração do transa
    Correção do return do mutação

--- teste.py
+++ teste.py
@@ -20,11 +20,26 @@ class conjuntoUPAs(object):
     def __init__(self, v = []):
         self.bairros = v
         self.fit = 10000
+
     def mutacao(self, c = cliente()):
-        temp = []
+        temp = conjuntoUPAs()
         for bairro in self.bairros:
-            temp.append(c.pos[rin(0,len(c.pos) - 1)])
+            temp.bairros.append(c.pos[rin(0,len(c.pos) - 1)])
+        temp.fitness(c)
         return temp
+
+    def fitness(self, c = cliente()):
+        fit = 0
+        bairro = 0
+        for i in range(len(c.pos)):
+            for k in self.bairros:
+                min = 10000
+                upaDist = pow(pow(k.x - c.pos[i].x, 2) + pow(k.y - c.pos[i].y, 2), 1/2)
+                if min > upaDist:
+                    min = upaDist
+                    bairro = i
+            fit += min
+        self.fit = fit
 
 
 class populacao(object):
@@ -34,48 +49,42 @@ class populacao(object):
         solucao = []
         self.c = c
         for j in range(tam):
-            for i in range(5):
+            for i in range(0,4):
                 solucao.append(c.pos[rin(0,len(c.pos) - 1)])
             temp = conjuntoUPAs(solucao)
-            temp.fit = self.fit(temp)
+            temp.fitness(self.c)
             self.upas.append(temp)
             solucao = []
-
-    def fit(self, cUPA = conjuntoUPAs()):
-            fit = 0
-            bairro = 0
-            for i in range(len(self.c.pos)):
-                for k in cUPA.bairros:
-                    min = 10000
-                    upaDist = pow(pow(k.x - self.c.pos[i].x, 2) + pow(k.y - self.c.pos[i].y, 2), 1/2)
-                    if min > upaDist:
-                        min = upaDist
-                        bairro = i
-                fit += min
-            return fit
-
-
+        self.upas = sorted(self.upas, key = lambda x: x.fit)
 
     def atualiza(self):
         upas = sorted(self.upas, key = lambda x: x.fit)
         temp = []
+        temp2 = []
         mut = 0.75
         for cUPA1 in upas:
             for cUPA2 in upas:
-                if uni(0,1) > 0.9:
-                    temp.append(cUPA1.mutacao)
+                if uni(0,1) > 1:
+                    temp.append(cUPA1.mutacao(self.c))
 
                 temp.append(self.transa(cUPA1, cUPA2))
+            temp.append(cUPA1)
+        temp = sorted(temp, key = lambda y: y.fit)
+        for i in range(len(upas) - 1):
+            temp2.append(temp[i])
+        self.upas = temp2
 
     def transa(self, c1 = conjuntoUPAs(), c2 = conjuntoUPAs()):
-        filho = []
-        for upa in range(len(c1) - 1): # cruzanmento dos pais
-            if rin(0,1) > 0.7 :
-                filho.append(c1.bairro[upa])
+        filho = conjuntoUPAs()
+        if len(c1.bairros) != len(c2.bairros):
+            return filho
+        for upa in range(0, len(c1.bairros) - 1): # cruzamento dos pais
+            if rin(0,1) > 0.7:
+                filho.bairros.append(c1.bairros[upa])
             else:
-                filho.append(c2.bairro[upa])
-
-        return conjuntoUPAs(filho)
+                filho.bairros.append(c2.bairros[upa])
+        filho.fitness(self.c)
+        return filho
 
 
 
@@ -99,13 +108,18 @@ plt.ylabel('y')
 plt.title('Distribuição de Bairros')
 plt.legend()
 plt.draw()
-
-pop = populacao(c)
-
-upax = []
-upay = []
-for i in pop.upas[0].bairros:
-    upax.append(i.x)
-    upay.append(i.y)
-plt.scatter(upax, upay, label = 'UPAs',marker = 'o')
 plt.show()
+
+pop = populacao(c, 5)
+for i in range(10):
+    upax = []
+    upay = []
+    for i in pop.upas[0].bairros:
+        upax.append(i.x)
+        upay.append(i.y)
+    pop.atualiza()
+    plt.scatter(upax, upay, label = 'UPAs',marker = 'o')
+    plt.draw()
+    plt.scatter(x, y, label = 'Bairros',marker = '*')
+    plt.draw()
+    plt.show()
