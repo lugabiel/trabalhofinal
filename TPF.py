ebab89f38646c371fb25ee68131f4933cc7006bc
Pedro Ian <pedroianmoraesus@gmail.com>
2019-06-20 17:09:50 -0300

    Create TPF.py
    teste para ver se está funcionando, o que por enquanto está, poreḿ, depois da primeira atualização, acho que há um erro, pois eles param de transar, ou seja, o tamanho dos bairros pais == 0

---
+++ TPF.py
@@ -0,0 +1,146 @@
+import matplotlib.pyplot as plt
+import random
+from random import randint as rin
+from random import uniform as uni
+import math
+from math import pow as pow
+import csv
+import time
+import tqdm
+
+class bairro(object):
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    def distancia(self, b):
+        dist = pow(self.x - b.x, 2)
+        dist += pow(self.y - b.y, 2)
+        return pow(dist, 1/2)
+
+class mapa(object):
+    def __init__(self):
+        self.bairros = []
+    def add(self, b = bairro()):
+        self.bairros.append(b)
+
+class conjuntoUPAs(object):
+    def __init__(self, m = mapa()):
+        self.bairrosUPAs = m
+        self.fit = 10000
+
+    def mutacao(self, m = mapa()):
+        temp = conjuntoUPAs()
+        for bairro in self.bairrosUPAs.bairros:
+            temp.bairrosUPAs.add(m.bairros[rin(0,len(m.bairros) - 1)])
+        temp.fitness(m)
+        return temp
+
+    def fitness(self, m = mapa()):
+        fit = 0
+        dist = 10000
+        if len(self.bairrosUPAs.bairros) > 10:
+            self.fit = dist
+            return
+        print(len(self.bairrosUPAs.bairros))
+        for i in m.bairros:
+            dist = 10000
+            for k in self.bairrosUPAs.bairros:
+                if k.distancia(i) < dist:
+                    dist = k.distancia(i)
+            fit += dist
+        print("fim de um fitness", len(m.bairros), len(self.bairrosUPAs.bairros))
+        self.fit = fit
+
+
+
+
+class populacao(object):
+    def __init__(self, m = mapa(), tam = 5):
+        self.upas = [] #lista de ponteiros para objetos conjuntoUPAs
+        self.selecao = [] #quais upas estao selecionadas para cada mapa
+        solucao = mapa()
+        self.m = m
+        self.tam = tam
+        for j in range(tam):
+            for i in range(5):
+                solucao.add(self.m.bairros[rin(0,len(self.m.bairros) - 1)])
+            temp = conjuntoUPAs(solucao)
+            solucao = mapa()
+            temp.fitness(self.m)
+            self.upas.append(temp)
+        self.upas = sorted(self.upas, key = lambda x: x.fit)
+
+    def atualiza(self):
+        upas = []
+        upas = sorted(self.upas, key = lambda x: x.fit)
+        upasFilho = []
+        print("upas 1 - ",len(upas))
+        for i in upas:
+            for k in self.upas:
+                upasFilho.append(self.transa(i, k))
+        upasFilho = sorted(upasFilho, key = lambda y: y.fit)
+        print("fim de uma atualiação", len(upasFilho), "-----------------------")
+        self.upas = []
+        for i in range(self.tam):
+            self.upas.append(upasFilho[i])
+        print("upas 2 - ", len(upas))
+
+    def transa(self, c1 = conjuntoUPAs(), c2 = conjuntoUPAs()):
+        filho = conjuntoUPAs()
+        filho.bairrosUPAs.bairros = []
+        tam1 = len(c1.bairrosUPAs.bairros)
+        tam2 = len(c2.bairrosUPAs.bairros)
+        if tam1 != tam2 or tam1 == 0:
+            for i in range(self.tam):
+                filho.bairrosUPAs.add(self.m.bairros[rin(0,len(self.m.bairros) - 1)])
+            filho.fitness(self.m)
+            return filho
+        else:
+            for i in range(self.tam):
+                if rin(0,1) > 0.9:
+                    filho.bairrosUPAs.add(c1.bairrosUPAs.bairros[i])
+                else:
+                    filho.bairrosUPAs.add(c2.bairrosUPAs.bairros[i])
+            print("fim de uma transa", len(self.m.bairros), len(c1.bairrosUPAs.bairros))
+            print("filho",len(filho.bairrosUPAs.bairros))
+        filho.fitness(self.m)
+        return filho
+
+
+
+m = mapa()
+x = []
+y = []
+
+
+
+with open('pmedian324.txt','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter = ' ')
+    for linha in plots:
+        m.bairros.append(bairro(int(linha[0]), int(linha[1])))
+        x.append(int(linha[0]))
+        y.append(int(linha[1]))
+
+plt.scatter(x, y, label = 'Bairros',marker = '*')
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.title('Distribuição de Bairros')
+plt.legend()
+plt.draw()
+plt.show()
+
+pop = populacao(m, 5)
+for k in range(10):
+#    upax = []
+#    upay = []
+#    for i in pop.upas[0].bairrosUPAs.bairros:
+#        upax.append(i.x)
+#        upay.append(i.y)
+    pop.atualiza()
+#    plt.scatter(upax, upay, label = 'UPAs',marker = 'o')
+#    plt.draw()
+#    plt.scatter(x, y, label = 'Bairros',marker = '*')
+#    plt.draw()
+#    plt.show()
+    time.sleep(1)
